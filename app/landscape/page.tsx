0e6f7bff0ce1213f656a553d31f726b7d182355b
Swarn Bawa <YOUR_ACTUAL_GITHUB_EMAIL>
2026-08-02 20:16:58 -0700

    Remove moved photo from landscape gallery

--- app/landscape/page.tsx
+++ app/landscape/page.tsx
@@ -44,7 +44,7 @@ export default function LandscapePage() {
         <Gallery
           title=""
           folder="landscape"
-          images={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]}
+          images={[1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16]}
         />
       </div>
         </main>
